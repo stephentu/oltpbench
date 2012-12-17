6e180b9ca3656bef722ebab84d8b2a21194c4c70
Stephen Tu <stephentu@csail.mit.edu>
2012-12-17 17:53:30 -0500

    xx

--- src/com/oltpbenchmark/benchmarks/ycsb/procedures/UpdateRecord.java
+++ src/com/oltpbenchmark/benchmarks/ycsb/procedures/UpdateRecord.java
@@ -8,11 +8,16 @@ import java.util.Map.Entry;
 
 import net.spy.memcached.MemcachedClient;
 
+import org.apache.log4j.Logger;
+
 import com.oltpbenchmark.api.Procedure;
 import com.oltpbenchmark.api.SQLStmt;
 import com.oltpbenchmark.benchmarks.ycsb.YCSBBenchmark;
+import com.oltpbenchmark.benchmarks.ycsb.YCSBConstants;
 
 public class UpdateRecord extends Procedure {
+
+    private static final Logger LOG = Logger.getLogger(ReadRecord.class);
     
     public final SQLStmt updateAllStmt = new SQLStmt(
         "UPDATE USERTABLE SET FIELD1=?,FIELD2=?,FIELD3=?,FIELD4=?,FIELD5=?," +
@@ -20,11 +25,6 @@ public class UpdateRecord extends Procedure {
     );
     
     public void run(Connection conn, MemcachedClient mcclient, int keyname, Map<Integer,String> vals) throws SQLException {
-        if (mcclient != null) {
-            // do mc invalidation
-            mcclient.delete(YCSBBenchmark.MCKey(keyname));
-        }
-        
     	PreparedStatement stmt = this.getPreparedStatement(conn, updateAllStmt);
 		assert(vals.size()==10);       
 		stmt.setInt(11,keyname); 
@@ -33,5 +33,15 @@ public class UpdateRecord extends Procedure {
         	stmt.setString(s.getKey(), s.getValue());
         }
         stmt.executeUpdate();
+
+        if (mcclient != null) {
+            try { 
+               // put the right record
+                mcclient.set(YCSBBenchmark.MCKey(keyname), YCSBConstants.MC_KEY_TIMEOUT, YCSBBenchmark.YCSBRecToJson(vals));
+            } catch (IllegalStateException e) {
+                // queue is too full
+                LOG.warn("MC queue is full", e);
+            }
+        }
     }
 }
